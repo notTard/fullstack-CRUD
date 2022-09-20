29dee8d33b216396ed96822e458e466659cc26c6
notTard <44521919+notTard@users.noreply.github.com>
2022-09-20 15:35:12 +0400

    Add files via upload
    Backend REST api

---
+++ springboot-backend-crud/src/test/java/com/crudpet/springboot/SpringbootBackendCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudpet.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
